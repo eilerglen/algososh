ba8af550991fecfede49390b73838688edf38a78
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-05-27 00:37:32 +0300

    add some logic of string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 
+type TForm = {
+  name: string;
+  value: string;
+};
 export const StringComponent: React.FC = () => {
+  const [form, setForm] = React.useState({});
+  console.log(form);
+
+  const onChange = (event: React.ChangeEvent<TForm>) => {
+    setForm({ ...form, [event.target.value]: event.target.value });
+  };
   return (
     <SolutionLayout title="Строка">
-     
+      <div>
+        <input type="text" onChange={onChange} />
+        <button> Развернуть</button>
+      </div>
+      <div>
+        {/* {form} */}
+      </div>
     </SolutionLayout>
   );
 };
