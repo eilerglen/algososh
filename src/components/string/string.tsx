f4d04961f259e10a4ae14651e91c4871adedb9fe
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-05-27 21:27:12 +0300

    first commit

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,25 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
 
-type TForm = {
-  name: string;
-  value: string;
-};
+import styles from "./string.module.css";
+
+
+interface symbolProps {
+  symbol: string;
+  state: ElementStates;
+}
+
 export const StringComponent: React.FC = () => {
-  const [form, setForm] = React.useState({});
-  console.log(form);
+  const [charArr, setCharArr] = useState<Array<symbolProps>>([]);
+  const [inProgress, setInProgress] = useState<boolean>(false);
 
-  const onChange = (event: React.ChangeEvent<TForm>) => {
-    setForm({ ...form, [event.target.value]: event.target.value });
-  };
+  //Изменить статус/внешний вид символов.
+  const changeState = (arr: any, status: string, start: number, end?: number) => {
+    arr[start].state = status;
+    if (end) {
+      arr[end].state = status;
+    }
+    setCharArr([...arr]);
+  }
+
+  //Перестановка двух символов.
+  const swap = (arr: Array<symbolProps>, leftInd: number, rightInd: number) => {
+    const temp = arr[leftInd];
+    arr[leftInd] = arr[rightInd];
+    arr[rightInd] = temp;
+  }
+
+  //Переставить символы с анимацией.
+  const recursion = async (arr: Array<symbolProps>) => {
+    setInProgress(true);
+    let end = arr.length - 1;
+    for (let start = 0; start <= end; start++) {
+      if (start === end) {
+        changeState(arr, ElementStates.Changing, start);
+        changeState(arr, ElementStates.Modified, start);
+      }
+      changeState(arr, ElementStates.Changing, start, end);
+      swap(arr, start, end);
+      changeState(arr, ElementStates.Modified, start, end);
+      end--;
+    }
+    setInProgress(false);
+  }
+
+  //Рендер вводимых символов.
+  const handleChange = (event: any) => {
+    setCharArr(event.target.value.split('').map((symbol: any) => {
+      return {
+        symbol: symbol,
+        state: ElementStates.Default
+      }
+    }))
+  }
+
+  //Запуск функции перестановки по нажатию кнопки.
+  const handleClick = async () => {
+    //Вернуть в дефолтное состояние, если строка уже отсортирована.
+    if (charArr[0].state === 'modified') {
+      setCharArr(charArr.map((symbol: any) => {
+        symbol.state = ElementStates.Default;
+        return symbol
+      }))
+    }
+    recursion(charArr);
+  }
+
   return (
     <SolutionLayout title="Строка">
-      <div>
-        <input type="text" onChange={onChange} />
-        <button> Развернуть</button>
+      <div className={`${styles['flex-container']}`}>
+
+        <Input
+          extraClass={styles.input}
+          isLimitText={true}
+          maxLength={11}
+          formOfWord={"символов"}
+          onChange={handleChange}
+        />
+
+        <Button text={'Развернуть'} onClick={handleClick} isLoader={inProgress}></Button>
+
       </div>
-      <div>
-        {/* {form} */}
+      <div className={`${styles['flex-container']}`}>
+        <ul className={styles.list}>
+          {charArr && charArr.map((el: symbolProps, ind) =>
+            <li className={`${styles['list-elem']}` } key={ind}>
+              <Circle
+                letter={el.symbol}
+                state={el.state}
+              />
+            </li>
+          )}
+        </ul>
       </div>
     </SolutionLayout>
   );
