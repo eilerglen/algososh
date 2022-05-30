af2e14514072061bca7f339f38b9f4845f93eac6
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-05-30 15:55:35 +0300

    add logic of checked radio-button

--- src/ui/radio-button/radio-button.tsx
+++ src/ui/radio-button/radio-button.tsx
@@ -1,33 +1,32 @@
-import React, {FC} from "react";
-import stylesRadioInput from './radio-button.module.css' 
+import React, { FC } from "react";
+import stylesRadioInput from "./radio-button.module.css";
+import { generateID } from "../../utils/generateId";
+import { nanoid } from "nanoid";
 
-interface RaduioButtonProps {
-    label: string,
-    name: string,
-    value: string,
-    onClick: () => void,
-    mixin: string
+interface RadioButtonProps extends React.HTMLProps<HTMLInputElement>{
+  label: string;
+  mixin: string;
+ 
 }
 
-export const RadioButton: FC<RaduioButtonProps> = ({
-    label = "Введите текст",
-    name, 
-    value,
-    onClick,
-    mixin}) => {
-    return (
-        <div className={`${stylesRadioInput.content} ${mixin}`}>
-          <input className={stylesRadioInput.input}
-             type="radio"
-             onClick={onClick}
-            />
-          <label className={`text text_type_button ${stylesRadioInput.label}`}>
-            {label}
-          </label>
-        </div>
-      );
-
-}
-
-  
-
+export const RadioButton: FC<RadioButtonProps> = ({
+  label = "Введите текст",
+  mixin="",
+  ...rest
+ 
+}) => {
+  const id = nanoid();
+  return (
+    <div className={`${stylesRadioInput.content} ${mixin}`}>
+      <input
+        className={stylesRadioInput.input}
+        id={id}
+        {...rest}
+      />
+      <label
+        className={`text text_type_button ${stylesRadioInput.label}`} htmlFor={id}>
+        {label}
+      </label>
+    </div>
+  );
+};
