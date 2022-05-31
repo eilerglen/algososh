91f60f3d6d2a82b82e3cbb67cc5e9a23dbd34982
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-06-01 00:09:10 +0300

    add some fixes of selection sort

--- src/utils/sortswitcher/selectionSort.ts
+++ src/utils/sortswitcher/selectionSort.ts
@@ -22,58 +22,49 @@ export const selectionSort = async (
   setInitialArr: React.Dispatch<React.SetStateAction<columnObject[]>>,
   initialArr: columnObject[]
 ) => {
-  //Копируем массив из стейта и делаем все элементы дефолтными
   const arr = [...initialArr];
-  arr.forEach((el) => (el.state = TStatusObject.Default));
   setInitialArr([...arr]);
   // Начинаем цикл
   const { length } = arr;
   for (let i = 0; i < length; i++) {
-    // Инициализация счётчика
     let swapInd = i;
-    // Подсвечиваем элемент рыжим, который будет отсортирован
     arr[i].state = TStatusObject.Chosen;
     overWriteArr(arr,  setInitialArr);
     await pause(1000)
-    // Начинаем цикл по оставшимся элементам
     for (let j = i + 1; j < length; j++) {
-      // Подсвечиваем кандидата на свап фиолетовым
       arr[j].state = TStatusObject.Changing;
       overWriteArr(arr,  setInitialArr);
       await pause(1000)
       if (
-        (mode === "ascending" ? arr[swapInd].num : arr[j].num) >
-        (mode === "ascending" ? arr[j].num : arr[swapInd].num)
+        (mode === "ascending" ? arr[swapInd].num : arr[j].num) > (mode === "ascending" ? arr[j].num : arr[swapInd].num)
       ) {
-        // Если кандидат больше (меньше) текущего экстремума - то мы нашли второй элемент на свап,
-        // подсвечиваем его рыжим, а старого кандидата либо делаем дефолтным,
-        // либо оставляем рыжим (если это i-й элемент, для которого мы ищем кандидата)
-        arr[j].state =  TStatusObject.Chosen;
+        arr[j].state =  TStatusObject.Chosen; //если у нас i-ый элемент(swapInd) больше j то делаем j-ый элемент желтым
         arr[swapInd].state =
-          i === swapInd ?  TStatusObject.Chosen : TStatusObject.Default;
-        swapInd = j;
+          i === swapInd ?  TStatusObject.Chosen : TStatusObject.Default; //задаем цвет столбика по такому правилу если у нас index i равен (swapInd) делаем его желтым цветом ,остальное дефолтным
+        swapInd = j; //начинаем переопределять индекс наименьшего элемента. Двигаем его вправо, если есть эелементы меньше до самого минимального
         overWriteArr(arr, setInitialArr);
         await pause(1000)
       }
-      // После визуальной сортировки меняем цвет текущего элемента, но не
-      // рисуем его (не сортируем массив) он будет отрисован на следующем шаге
       arr[j].state =
         swapInd === j ? TStatusObject.Chosen :  TStatusObject.Default;
     }
-    // Если сортируемый элемент сам является экстремумом - рисуем его как "modified"
+
     if (i === swapInd) {
+      //Если у нас i-ый элемент оказался минимальным, то соотвествующий столбик красим в зеленый
       arr[i].state =  TStatusObject.Modified;
       overWriteArr(arr, setInitialArr);
       await pause(1000)
     }
    
     else {
-      swap(arr, i, swapInd);
+       //Если же нет, то
+      swap(arr, i, swapInd); //меняем i-ый элемент местами с минимальным
       await overWriteArr(arr, setInitialArr);
-      arr[i].state =  TStatusObject.Modified;
-      arr[swapInd].state = TStatusObject.Default;
-      await overWriteArr(arr,  setInitialArr);
+      arr[i].state =  TStatusObject.Modified; //i-ый элемент: соотвествующий столбик красим в зеленый
+      arr[swapInd].state = TStatusObject.Default; 
     }
   }
 
 };
+
+
